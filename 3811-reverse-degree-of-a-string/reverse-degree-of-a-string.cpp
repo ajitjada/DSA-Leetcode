cdc6c3e364f0134edc69602b3b5f662f1e299fd0
Ajit Jada <ajitjada71@gmail.com>
2026-09-20 09:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (5.64%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n = s.length();
+        vector<int> reverseA(n, 0);
+
+        for(int i=0; i<n; i++) {
+            reverseA[i] = 'z' - s[i] + 1;
+        }
+
+        int sum = 0;
+
+        for(int i=1; i<=n; i++) {
+            sum = sum + (i * reverseA[i-1]);
+        }
+
+        return sum;
+    }
+};
